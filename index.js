1b0c0d8d1500f51ff91aceae34126bd09e87fe05
1yumeng-asd <fuxiaoxun2@gmail.com>
2026-03-22 16:15:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 export default {
-  async fetch(request, env, ctx) {
-    return new Response("Hello jiuguan-helper!", {
-      headers: { "Content-Type": "text/plain" }
-    });
+  async fetch(request) {
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    };
+
+    if (request.method === "OPTIONS") {
+      return new Response(null, { headers });
+    }
+
+    return new Response(JSON.stringify({
+      code: 200,
+      msg: "云端连接成功",
+      autoPlayInterval: 3000,
+      maxPlayTimes: 10
+    }), { headers });
   }
 };
